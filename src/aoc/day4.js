ee432ae51a8911a5a1ffd1327887d150e3be7107
Lenart Velkavrh <lenart.velkavrh@poviolabs.com>
2019-08-13 13:58:32 +0200

    wip: find which minute most asleep

--- src/aoc/day4.js
+++ src/aoc/day4.js
@@ -16,19 +16,58 @@ const testInput = `[1518-11-01 00:00] Guard #10 begins shift
 [1518-11-05 00:45] falls asleep
 [1518-11-05 00:55] wakes up`;
 
-const parse = i =>
+const getParse = i =>
   i.split(/\n/).map(l => {
-    const [year, month, day, hour, minute] = l.substr(1, 16).split(/\D/);
+    let [year, month, day, hour, minute] = l.substr(1, 16).split(/\D/);
     const rest = l.substr(19);
+    if (parseInt(hour) > 1) {
+      day = (parseInt(day) + 1).toString();
+      hour = "00";
+      minute = "00";
+    }
     return {
       year,
       month,
       day,
       hour,
       minute,
-      date: new Date(Date.UTC(year, month, day, hour, minute)),
+      date: new Date(Date.UTC(year, month - 1, day, hour, minute)),
+      guardId: parseInt(rest.replace(/\D/g, "")),
+      asleep: rest === "falls asleep",
       rest
     };
   });
 
-console.log(parse(testInput));
+const getSort = i => i.sort((a, b) => a.date.getTime() - b.date.getTime());
+
+const getRanges = i =>
+  i.reduce((acc, val, index, self) => {
+    if (!Number.isNaN(val.guardId)) {
+      acc.push({ date: val.date, guardId: val.guardId, ranges: [] });
+      return acc;
+    }
+    if (!val.asleep) {
+      const prev = self[index - 1];
+      acc[acc.length - 1].ranges.push({ start: prev.date, end: val.date });
+    }
+
+    return acc;
+  }, []);
+
+const addTimeMostAsleep = i =>
+  i.map(val => {
+    const minutesAsleep = val.ranges.reduce(
+      (aslp, range) =>
+        aslp + (range.end.getTime() - range.start.getTime()) / 1000 / 60,
+      0
+    );
+    return { ...val, minutesAsleep };
+  }, []);
+
+const addMinuteMostAsleep = i => i.sort((a, b) => a.guardId - b.guardId);
+
+const res = addMinuteMostAsleep(
+  addTimeMostAsleep(getRanges(getSort(getParse(testInput))))
+);
+console.log("res", res);
+// console.log(getSort(getParse(testInput)));
