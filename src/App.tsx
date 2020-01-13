141a7a29da9fcad72e2e1651575cba06ce5b870f
Lenart Velkavrh <lenart.velkavrh@poviolabs.com>
2020-01-13 11:51:30 +0100

    properly set vars

--- src/App.tsx
+++ src/App.tsx
@@ -19,12 +19,17 @@ interface State {
   };
 }
 
-export default class extends React.Component<{}, State> {
-  state = {
-    map: { lng: 5, lat: 34, zoom: 2 }
-  };
+interface ExtendedLayer extends mapboxgl.CustomLayerInterface {
+  program: WebGLProgram | null;
+  aPos: number;
+  buffer: WebGLBuffer | null;
+  type: any;
+}
 
-  mapState = { lng: 5, lat: 34, zoom: 2 };
+export default class extends React.Component {
+  highlightProgram: WebGLProgram | null = null;
+  highlightAPos: number = 0;
+  highlightBuffer: WebGLBuffer | null = null;
 
   mapContainer = React.createRef<HTMLDivElement>();
 
@@ -35,24 +40,150 @@ export default class extends React.Component<{}, State> {
       this.map = new mapboxgl.Map({
         container: this.mapContainer.current,
         style: "mapbox://styles/mapbox/streets-v11",
-        center: [this.state.map.lng, this.state.map.lat],
-        zoom: this.state.map.zoom
+        center: [5, 34],
+        zoom: 2
       });
     }
-    this.map?.on("move", () => {
-      if (!this.map) {
-        return;
-      }
-      this.mapState = {
+    this.setMapEvents();
+    // this.map?.on("move", () => {
+    //   if (!this.map) {
+    //     return;
+    //   }
+    //   this.mapState = {
+    //     lng: this.map.getCenter().lng,
+    //     lat: this.map.getCenter().lat,
+    //     zoom: this.map.getZoom()
+    //   };
+    // });
+  }
+
+  get mapState() {
+    if (this.map) {
+      return {
         lng: this.map.getCenter().lng,
         lat: this.map.getCenter().lat,
         zoom: this.map.getZoom()
       };
-    });
+    }
+    return {
+      lng: 0,
+      lat: 0,
+      zoom: 0
+    };
   }
 
+  setMapEvents = () => {
+    if (!this.map) {
+      return;
+    }
+    this.map.on("load", () => {
+      // @ts-ignore
+      this.map && this.map.addLayer(this.highlightLayer, "building");
+    });
+  };
+
+  // create a custom style layer to implement the WebGL content
+  highlightLayer: ExtendedLayer = {
+    id: "highlight",
+    type: "custom",
+    program: null,
+    aPos: 0,
+    buffer: null,
+
+    // method called when the layer is added to the map
+    // https://docs.mapbox.com/mapbox-gl-js/api/#styleimageinterface#onadd
+    onAdd: function(map: mapboxgl.Map, gl: WebGL2RenderingContext) {
+      // create GLSL source for vertex shader
+      var vertexSource =
+        "" +
+        "uniform mat4 u_matrix;" +
+        "attribute vec2 a_pos;" +
+        "void main() {" +
+        "    gl_Position = u_matrix * vec4(a_pos, 0.0, 1.0);" +
+        "}";
+
+      // create GLSL source for fragment shader
+      var fragmentSource =
+        "" +
+        "void main() {" +
+        "    gl_FragColor = vec4(1.0, 0.0, 0.0, 0.5);" +
+        "}";
+
+      // create a vertex shader
+      const vertexShader = gl.createShader(gl.VERTEX_SHADER);
+      if (vertexShader) {
+        gl.shaderSource(vertexShader, vertexSource);
+        gl.compileShader(vertexShader);
+      }
+
+      // create a fragment shader
+      const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+      if (fragmentShader) {
+        gl.shaderSource(fragmentShader, fragmentSource);
+        gl.compileShader(fragmentShader);
+      }
+
+      // link the two shaders into a WebGL program
+      this.program = gl.createProgram();
+      if (this.program && vertexShader && fragmentShader) {
+        gl.attachShader(this.program, vertexShader);
+        gl.attachShader(this.program, fragmentShader);
+        gl.linkProgram(this.program);
+        this.aPos = gl.getAttribLocation(this.program, "a_pos");
+      }
+
+      // define vertices of the triangle to be rendered in the custom style layer
+      var helsinki = mapboxgl.MercatorCoordinate.fromLngLat({
+        lng: 25.004,
+        lat: 60.239
+      });
+      var berlin = mapboxgl.MercatorCoordinate.fromLngLat({
+        lng: 13.403,
+        lat: 52.562
+      });
+      var kyiv = mapboxgl.MercatorCoordinate.fromLngLat({
+        lng: 30.498,
+        lat: 50.541
+      });
+
+      // create and initialize a WebGLBuffer to store vertex and color data
+      this.buffer = gl.createBuffer();
+      gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer);
+      gl.bufferData(
+        gl.ARRAY_BUFFER,
+        new Float32Array([
+          helsinki.x,
+          helsinki.y,
+          berlin.x,
+          berlin.y,
+          kyiv.x,
+          kyiv.y
+        ]),
+        gl.STATIC_DRAW
+      );
+    },
+
+    // method fired on each animation frame
+    // https://docs.mapbox.com/mapbox-gl-js/api/#map.event:render
+    render: function(gl: WebGL2RenderingContext, matrix: Iterable<number>) {
+      if (this.program) {
+        gl.useProgram(this.program);
+        gl.uniformMatrix4fv(
+          gl.getUniformLocation(this.program, "u_matrix"),
+          false,
+          matrix
+        );
+      }
+      gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer);
+      gl.enableVertexAttribArray(this.aPos);
+      gl.vertexAttribPointer(this.aPos, 2, gl.FLOAT, false, 0, 0);
+      gl.enable(gl.BLEND);
+      gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+      gl.drawArrays(gl.TRIANGLE_STRIP, 0, 3);
+    }
+  };
+
   render() {
-    console.log("r");
     return (
       <div ref={this.mapContainer} className="mapContainer">
         Map
